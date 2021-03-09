7c42dbf1520f49f9d5c27c95d7c5f5ecfd35f1c1
Austin Guiney <aguiney@cs.umb.edu>
2021-03-09 14:33:36 -0500

    I got it to count consecutive characters.

--- mp2/part1/count.s
+++ mp2/part1/count.s
@@ -5,20 +5,23 @@ count:
     movl %esp,%ebp # Save %esp into #ebp.
     subl $8, %esp # Set up local automatic variables
 
-    movl 12(%ebp), %edx # Move the first character of the string into edx.
+    movl 8(%ebp), %edx # Move the first character of the string into edx.
+
     xorl %ecx, %ecx # Move 0 into %ecx
 
 loop:
     movzbl (%edx), %eax
-    cmpb $97, %eax
-    je done
+    cmpb $97, %al
+    je increment
+    jmp done
+
+increment:
     addl $1, %ecx
     addl $1, %edx
     jmp loop
 
 done:
-    xorl %eax, %eax
-    movl %ecx, %eax
+    movl %ecx, %eax # Move the counter into eax
     movl %ebp,%esp # restore %esp from %ebp
     popl %ebp # restore %ebp
     ret # Return to the calling function
