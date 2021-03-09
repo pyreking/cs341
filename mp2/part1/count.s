e3555c9b19a1bcfb8cd2bc988276f518fb93be7a
Austin Guiney <aguiney@cs.umb.edu>
2021-03-09 13:22:40 -0500

    Making some headway.

--- mp2/part1/count.s
+++ mp2/part1/count.s
@@ -5,11 +5,20 @@ count:
     movl %esp,%ebp # Save %esp into #ebp.
     subl $8, %esp # Set up local automatic variables
 
-    mov 8(%ebp), %edx # Move the first character of the string into edx.
-    addl $1, %edx
+    movl 12(%ebp), %edx # Move the first character of the string into edx.
+    xorl %ecx, %ecx # Move 0 into %ecx
 
+loop:
     movzbl (%edx), %eax
+    cmpb $97, %eax
+    je done
+    addl $1, %ecx
+    addl $1, %edx
+    jmp loop
 
+done:
+    xorl %eax, %eax
+    movl %ecx, %eax
     movl %ebp,%esp # restore %esp from %ebp
     popl %ebp # restore %ebp
     ret # Return to the calling function
