c73b7eaf3cb3ee67b2f8acf2dcb3008fc70c6bfd
Austin Guiney <aguiney@cs.umb.edu>
2021-03-22 19:13:54 -0400

    Finish strncpy.s more or less.

--- mp2/part2+3/strncpy.s
+++ mp2/part2+3/strncpy.s
@@ -4,43 +4,34 @@
 mystrncpy:
     pushl %ebp # Set up stack frame.
 	movl %esp, %ebp # Save %esp into %ebp.
-	movl 8(%ebp), %eax # Copy the destination string into %eax.
-	movl 12(%ebp), %edx # Copy the source string into %edx.
-    xorl %ecx, %ecx # Clear out %ecx for counting.
+    subl $12, %esp
+
+	movl 8(%ebp), %ecx # Copy the destination string into %eax.
+	movl 12(%ebp), %eax # Copy the source string into %edx.
+
+    xor %edx, %edx
 
 loop1:
-    movb (%edx), %dl # Move the current character in %edx to %dl.
-    movb (%edx), %eax # Move the current character into the current address of %eax.
-    addl $1, %ecx # Increment the character counter by 1.
+    cmpb %dh, 16(%ebp) # Compare %dh with the number of characters to copy.
+    je done # Jump to done if the numbers are the same.
 
-    # cmpb $0, %dl # Check for the null terminator in the source string.
-    # jz done # Jump to done if there are no more characters to copy.
+    movb (%eax), %dl # Move the current character in %eax to %dl.
+    movb %dl, (%ecx) # Move the current character into the current address of %ecx.
 
-    # cmpb $0, %al # Check for the null terminator in the destination string.
-    # jz done # Jump to done if there are no more characters to copy.
+    add $1, %dh # Use %dh as a character counter.
 
-    cmpl %ecx, 16(%ebp) # Compare the character counter to the number of characters to copy.
-    je done # Jump to done if all of the characters have been copied.
+    cmpb $0, %dl # Look for a null terminator in %dl.
+    jz done # Jump to done if a null terminator is found.
     
     jmp increment # Jump to increment otherwise.
 
-# loop2:
-    # cmpl %ecx, 16(%ebp) # Compare the character counter to the number of characters to copy.
-    # je done # Jump to done if all of the characters have been copied.
-
-    # cmpb $0, %al # Check for a null terminator in the destination string.
-    # jz done  # Jump to done if the current character in the destination string is a null character.
-
-    # movb $0, %al # Fill the current character in the destination string with a null character.
-    # incl %eax # Move the destination string forward by 1.
-    # jmp loop2 # Jump to loop2.
-
 increment:
-    incl %edx # Move the source string forward by one character.
-    incl %eax # Move the destination string forward by one character.
+    addl $1, %eax # Move the destination string forward by one character.
+    addl $1, %ecx # Move the source string forward by one character.
     jmp loop1 # Jump back to the main loop.
 
 done:
+    movb $0, (%ecx)
     movl 8(%ebp), %eax # Move the copied string into eax.
     movl %ebp, %esp # Restore %esp from %ebp.
 	popl %ebp # Restore %ebp.
