af218b70c30019077a5f18ef0e19e6862860ebb9
Austin Guiney <aguiney@cs.umb.edu>
2021-03-25 17:16:07 -0400

    I have a non-working prototype.

--- mp2/part2+3/printbin.s
+++ mp2/part2+3/printbin.s
@@ -1,11 +1,45 @@
-   .text
-    .globl mystrncpy
+   .data
+
+bin:
+    .asciz "0000 0000"
+    .text
+    .globl printbin
 
 printbin:
     pushl %ebp # Set up stack frame.
 	movl %esp, %ebp # Save %esp into %ebp.
     subl $4, %esp # Set up local automatic variable
 
-	movl 8(%ebp), %edx # Copy the destination string into %eax.
+	movb 8(%ebp), %dl # Copy the hex value to %dl
+    movb %dl, %dh # Copy the hex value to $dh
+    shrb $4, %dl # Get the first nibble
+    and $0x0F, %dh # Get the second nibble
+
+    movl $bin, %eax # Move the default string to %eax
+
+    xorl %ecx, %ecx
+
+    call donibble # Process the first dibble
+    jmp done # Jump to done
+
+donibble:
+    cmpb $4, %ch
+
+    movb $0x09, %cl
+
+    sarb %ch, %cl
+    and %dl, %cl
+
+    cmbp $1, %cl
+    
+
+    cmpb $0, %cl
 
     
+    # movb $'1', (%eax)
+
+done:
+    movl %ebp, %esp # Restore %esp from %ebp.
+	popl %ebp # Restore %ebp.
+	ret # Return to the calling function.
+	.end # End the program
