f49acd9feb0065b0df4792ee12d4dd9c1af80b1e
Austin Guiney <aguiney@cs.umb.edu>
2021-03-26 17:01:03 -0400

    Finished printbin.s.

--- mp2/part2+3/printbin.s
+++ mp2/part2+3/printbin.s
@@ -11,7 +11,8 @@ printbin:
     subl $4, %esp # Set up local automatic variable
 
 	movb 8(%ebp), %dl # Copy the hex value to %dl
-    shrb $4, %dl # Get the first nibble
+    
+    sarb $4, %dl # Get the first nibble
     movb %dl, %dh # Copy the hex value to $dh
 
     movl $bin, %eax # Move the default string to %eax
@@ -20,7 +21,23 @@ printbin:
     movb $3, %cl
     movb $0x8, %ch
 
-    call donibble # Process the first dibble
+    call donibble
+
+    addl $1, %eax
+
+    xorl %ecx, %ecx
+    movb $3, %cl
+    movb $0x8, %ch
+
+    xorl %edx, %edx
+
+    movb 8(%ebp), %dl
+    andb $0xF, %dl
+
+    movb %dl, %dh
+    
+    call donibble
+
     jmp done # Jump to done
 
 donibble:
@@ -45,7 +62,7 @@ exit:
     ret
 
 done:
-    subl $4, %eax
+    subl $9, %eax
     movl %ebp, %esp # Restore %esp from %ebp.
 	popl %ebp # Restore %ebp.
 	ret # Return to the calling function.
