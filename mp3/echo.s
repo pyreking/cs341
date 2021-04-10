ed2ab03632d30517718391770866c6a278bb2699
Austin Guiney <aguiney@cs.umb.edu>
2021-04-10 18:59:55 -0400

    Removed hardcoded port addresses.

--- mp3/echo.s
+++ mp3/echo.s
@@ -6,15 +6,18 @@ echo:
 	movl %esp, %ebp # Save %esp into %ebp.
     subl $8, %esp # Set up local automatic variables.
 
-    movb 8(%ebp), %dl # Put the serial port into %dl.
-    movb 12(%ebp), %bl # Put the escape character into %al.
+    movl 8(%ebp), %edi # Put the serial port into %edi.
+    movb 12(%ebp), %bl # Put the escape character into %bl.
 
     xorl %ecx, %ecx # Clear out the %ecx register.
-    movw $0x2fc, %dx # Modem control.
+
+    lea 4(%edi), %edx # Modem control.
+
     inb (%dx), %al	# Get current.
     orb $0x03, %al	# Or on 2 lsbs.
     outb %al, (%dx)	# Set control.
-    movw $0x2fe, %dx # Modem status.
+    
+    lea 6(%edi), %edx # Modem status.
 
 loop1:
     inb (%dx), %al # Get current.
@@ -23,23 +26,26 @@ loop1:
     jnz loop1 # Go back to the beginning if some missing.
 
 loop2:
-    movw $0x2fd, %dx # Line status.
+    lea 5(%edi), %edx # Line status.
     inb (%dx), %al # Get data ready.
     andb $0x01, %al # Look at data read.
     jz loop2 # Go back to the beginning if recv data.
-    movw $0x2f8, %dx # I/O data address.
+
+    movl %edi, %edx # I/O data address.
     inb (%dx), %al # Move RX to %al.
     movb %al, %cl # Save RX to %cl.
     cmpb %cl, %bl # Check for escape character.
     je done # Jump to done if escape character has been found.
-    movw $0x2fd, %dx # Line status.
+
+    lea 5(%edi), %edx # Line status.
 
 xmit:
     inb (%dx), %al # Get THRE.
     andb $0x20, %al	# Look at THRE.
     jz loop2 # Go back to loop2 if TX HR empty.
+    
     movb %cl, %al # Get data to send.
-    movw $0x2f8, %dx # I/O data address.
+    movl %edi, %edx # I/O data address.
     outb %al, (%dx) # Send it.
     jmp loop2 # Go back to loop2 after transmitting the data.
 
